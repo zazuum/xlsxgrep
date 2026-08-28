aed40716fd8fc44f4cf5b880f73ce86709a2ee8d
zazuum <cviticivan@gmail.com>
2026-08-28 23:22:35 +0200

    version attribute from xlsxgrep.py

--- xlsxgrep/__init__.py
+++ xlsxgrep/__init__.py
@@ -1,1 +1,5 @@
 # This file is part of xlsxgrep, CLI tool to search text in XLSX, XLS, CSV, TSV and ODS files. It works similarly to Unix/GNU Linux grep.
+
+from .xlsxgrep import __version__
+
+__all__ = ["__version__"]
