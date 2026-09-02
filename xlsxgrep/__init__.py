2d7fb3649ba38edb1e92755e94333d81943f0028
zazuum <cviticivan@gmail.com>
2026-09-02 12:10:26 +0200

    Import process_single_file, SEARCH.

--- xlsxgrep/__init__.py
+++ xlsxgrep/__init__.py
@@ -1,5 +1,5 @@
 # This file is part of xlsxgrep, CLI tool to search text in XLSX, XLS, CSV, TSV and ODS files. It works similarly to Unix/GNU Linux grep.
 
-from .xlsxgrep import __version__
+from .xlsxgrep import __version__, process_single_file, SEARCH
 
-__all__ = ["__version__"]
+__all__ = ["__version__", "process_single_file", "SEARCH"]
